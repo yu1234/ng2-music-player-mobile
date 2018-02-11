878409d64de7bf9691f6a89a13cf8bbedba4c6f8
yuliu <836986488@qq.com>
2018-02-11 22:01:31 +0800

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,45 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {ErrorHandler, NgModule} from '@angular/core';
+import {Pro} from '@ionic/pro';
+import {ErrorHandler, Injectable, Injector, NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {StatusBar} from '@ionic-native/status-bar';
-import { Toast } from '@ionic-native/toast';
-import { NativeStorage } from '@ionic-native/native-storage';
-import { NativeAudio } from '@ionic-native/native-audio';
-import { Media } from '@ionic-native/media';
+import {Toast} from '@ionic-native/toast';
+import {NativeStorage} from '@ionic-native/native-storage';
+import {NativeAudio} from '@ionic-native/native-audio';
+import {Media} from '@ionic-native/media';
 
 
 import {MyApp} from './app.component';
 import {CommonProvider} from '../providers/common/common';
-import { AjaxProvider } from '../providers/ajax/ajax';
-import { GlobalProvider } from '../providers/global/global';
-import { AudioProvider } from '../providers/audio/audio';
+import {AjaxProvider} from '../providers/ajax/ajax';
+import {GlobalProvider} from '../providers/global/global';
+import {AudioProvider} from '../providers/audio/audio';
+
+Pro.init('237a2431!', {
+  appVersion: '0.0.1'
+})
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch (e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
 
 @NgModule({
   declarations: [
